1abbd646c263ad456bdc022cc5de434662d3ef79
mentalveritas <mentalveritas@gmail.com>
2016-07-10 01:07:15 +0900

    Update email valid check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase }
+	#before_save {email.downcase!}
   validates :name,  presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ 
+	#naver..com 구분을 못해준다 
+	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#구분하는 새로운 것
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
